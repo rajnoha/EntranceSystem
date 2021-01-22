9e6fc03c471a9afa98645382d0dc904dcc83a74c
Iv <rajenko@centrum.sk>
2021-01-22 15:36:13 +0100

    pridany folder v resources a do neho vlozene xml-ko

--- src/main/java/sk/rajnoha/EntranceSystem/App.java
+++ src/main/java/sk/rajnoha/EntranceSystem/App.java
@@ -3,6 +3,8 @@ package sk.rajnoha.EntranceSystem;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
@@ -12,20 +14,23 @@ import javafx.stage.Stage;
 public class App extends Application
 {
 	public void start(Stage stage) throws Exception {
-		Button button = new Button("stlač ma!");
+		Parent rootPane = FXMLLoader.load(
+				getClass().getResource("MainWindow.fxml"));
+		
+		//Button button = new Button("stlač ma!");
 		//button.setOnAction(new SysoutHandler());
 		//setOnAction potrebuje EventHandler, a tu je podany ako interface- anonymna trieda
-		button.setOnAction(new EventHandler<ActionEvent>() {
+		//button.setOnAction(new EventHandler<ActionEvent>() {
 			
-			@Override
-			public void handle(ActionEvent event) {
-				System.out.println("Anonymny klik");
+		//	@Override
+			//public void handle(ActionEvent event) {
+			//	System.out.println("Anonymny klik");
 				
-			}
-		});
-		AnchorPane rootPane = new AnchorPane();
-		rootPane.getChildren().add(button);
-		rootPane.setPrefSize(400, 300);
+			//}
+		//});
+		//AnchorPane rootPane = new AnchorPane();
+		//rootPane.getChildren().add(button);
+		//rootPane.setPrefSize(400, 300);
 		Scene scene = new Scene(rootPane);
 		stage.setTitle("Hello World");
 		stage.setScene(scene);
